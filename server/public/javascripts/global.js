528b73bd7b6b16b492e2cc74dd8b0adf8ab8eea9
e2l3n <toma.todorov@gmail.com>
2015-05-08 01:45:19 +0300

    Implemented client details section.

--- server/public/javascripts/global.js
+++ server/public/javascripts/global.js
@@ -1,7 +1,10 @@
 
+var availableClients = [];
+
 // DOM Ready =============================================================
 $(document).ready(function() {
-
+   // Client details link click
+    $('#clientList table tbody').on('click', 'td a.linkshowdetails', showClientInfo);
     // Populate the clients table on initial page load
     displayClients();
 });
@@ -10,15 +13,15 @@ $(document).ready(function() {
 
 // Fill table with data
 function displayClients() {
-
     // Empty content string
     var tableContent = '';
     // jQuery AJAX call for JSON
     $.getJSON( '/clients/discovered', function( data ) {
+	availableClients = data;
         // For each item in JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.name + '">' + this.name + '</a></td>';
+            tableContent += '<td><a href="#" class="linkshowdetails" rel="' + this.addresses[0] + '">' + this.name + '</a></td>';
             tableContent += '<td>' + this.addresses[0] + '</td>';
             tableContent += '<td>' + this.port + '</td>';
             tableContent += '</tr>';
@@ -28,3 +31,32 @@ function displayClients() {
         $('#clientList table tbody').html(tableContent);
     });
 };
+
+// Show Client Info
+function showClientInfo(event) {
+    // Prevent link from firing
+    event.preventDefault();
+    // Retrieve ip address from link rel attribute
+    var thisIPAddress = $(this).attr('rel');
+    // Get index of object based on id value
+    var arrayPosition = availableClients.map(function(arrayItem) { return arrayItem.addresses[0]; }).indexOf(thisIPAddress);
+    // Get client object
+    var thisClientObject = availableClients[arrayPosition];
+
+    //Populate info box
+    $('#clientInfoFullname').text(thisClientObject.fullname);
+    $('#clientInfoHost').text(thisClientObject.host);
+    $('#clientInfoIP').text(thisClientObject.addresses[0]);
+    $('#clientInfoPort').text('' + thisClientObject.port);
+};
+
+
+
+
+
+
+
+
+
+
+
--- server/routes/clients.js
+++ server/routes/clients.js
@@ -5,7 +5,12 @@ var router = express.Router();
  * GET discovered clients.
 */
 router.get('/discovered', function(req, res) {
-   res.json(req.discovered_clients);
+	//if (req.discovered_clients) {
+		//res.writeHead(200, {'Content-Type': 'application/json'});
+		res.json(req.discovered_clients);	
+	//} else {
+	//	res.writeHead(404);
+	//}   	
 });
 
 module.exports = router;
